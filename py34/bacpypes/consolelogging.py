85a8ce955818c7bd9288b6da752f0357534d0dfe
Joel Bender <jjb5@cornell.edu>
2015-08-02 22:17:11 -0400

    fixes #10

--- py34/bacpypes/consolelogging.py
+++ py34/bacpypes/consolelogging.py
@@ -106,8 +106,7 @@ class ArgumentParser(argparse.ArgumentParser):
 
         # check to dump labels
         if result_args.buggers:
-            loggers = logging.Logger.manager.loggerDict.keys()
-            loggers.sort()
+            loggers = sorted(logging.Logger.manager.loggerDict.keys())
             for loggerName in loggers:
                 sys.stdout.write(loggerName + '\n')
             sys.exit(0)
