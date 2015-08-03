f01c0cab176371ce0a4cdef5eef429f662d4ba51
Joel Bender <jjb5@cornell.edu>
2015-08-03 16:00:41 -0400

    empty shell for testing state machines

---
+++ tests/test_utilities/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+Test Utilities
+--------------
+
+This module tests the test utilities.
+"""
+
+from . import test_state_machine
+from . import test_client_state_machine
+from . import test_server_state_machine
