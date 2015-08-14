a3c95161c4e517de081533d440bcbf6e905e329d
Christian Tremblay, ing <christian.tremblay@servisys.com>
2015-08-13 21:43:15 -0400

    Added AccessPointObject tests
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

---
+++ tests/test_objects/test_AccessPointObject.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Nose Test Objects AccessPointObject
+---------------------
+"""
+
+import unittest
+from helper import TestObjectHelper
+
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger, xtob
+from bacpypes.object import AccessPointObject, WritableProperty, ReadableProperty, OptionalProperty
+from bacpypes.primitivedata import BitString, Boolean, CharacterString, Date, Double, \
+    Enumerated, Integer, Null, ObjectIdentifier, OctetString, Real, Time, \
+    Unsigned
+from bacpypes.constructeddata import AnyAtomic, Array, ArrayOf, Choice, Element, \
+    Sequence, SequenceOf
+from bacpypes.basetypes import AccessCredentialDisable, AccessCredentialDisableReason, \
+    AccessEvent, AccessPassbackMode, AccessRule, AccessThreatLevel, \
+    AccessUserType, AccessZoneOccupancyState, AccumulatorRecord, Action, \
+    ActionList, AddressBinding, AssignedAccessRights, AuthenticationFactor, \
+    AuthenticationFactorFormat, AuthenticationPolicy, AuthenticationStatus, \
+    AuthorizationException, AuthorizationMode, BackupState, BinaryPV, \
+    COVSubscription, CalendarEntry, ChannelValue, ClientCOV, \
+    CredentialAuthenticationFactor, DailySchedule, DateRange, DateTime, \
+    Destination, DeviceObjectPropertyReference, DeviceObjectReference, \
+    DeviceStatus, DoorAlarmState, DoorSecuredStatus, DoorStatus, DoorValue, \
+    EngineeringUnits, EventNotificationSubscription, EventParameter, \
+    EventState, EventTransitionBits, EventType, FaultParameter, FaultType, \
+    FileAccessMethod, LifeSafetyMode, LifeSafetyOperation, LifeSafetyState, \
+    LightingCommand, LightingInProgress, LightingTransition, LimitEnable, \
+    LockStatus, LogMultipleRecord, LogRecord, LogStatus, LoggingType, \
+    Maintenance, NetworkSecurityPolicy, NodeType, NotifyType, \
+    ObjectPropertyReference, ObjectTypesSupported, OptionalCharacterString, \
+    Polarity, PortPermission, Prescale, PriorityArray, ProcessIdSelection, \
+    ProgramError, ProgramRequest, ProgramState, PropertyAccessResult, \
+    PropertyIdentifier, Recipient, Reliability, RestartReason, Scale, \
+    SecurityKeySet, SecurityLevel, Segmentation, ServicesSupported, \
+    SetpointReference, ShedLevel, ShedState, SilencedState, SpecialEvent, \
+    StatusFlags, TimeStamp, VTClass, VTSession, WriteStatus
+from bacpypes.errors import ConfigurationError, ExecutionError
+
+# some debugging
+_debug = 1
+_log = ModuleLogger(globals())
+
+@bacpypes_debugging
+class Test_AccessPointObject(unittest.TestCase, TestObjectHelper):
+    """
+    This test will verify that the object created has good number of properties
+    Will test that each property is correct datatype
+    Will test that only Writable property can be written
+    Will test that all properties can be read
+    """
+    def setUp(self):
+        if _debug: Test_AccessPointObject._debug("Test_AccessPointObject")
+        self.obj = AccessPointObject()
+        self.objType = 'accessPoint'
+        self.identifiers = self.build_list_of_identifiers(self.obj.properties)
+        self.numberOfPropertiesRequired = 46
+        self.writeValue = 0
+        self.listOfProperties = \
+        [ (ReadableProperty,'statusFlags', StatusFlags)
+        , (ReadableProperty,'eventState', EventState)
+        , (ReadableProperty,'reliability', Reliability)
+        , (ReadableProperty,'outOfService', Boolean)
+        , (ReadableProperty,'authenticationStatus', AuthenticationStatus)
+        , (ReadableProperty,'activeAuthenticationPolicy', Unsigned)
+        , (ReadableProperty,'numberOfAuthenticationPolicies', Unsigned)
+        , (OptionalProperty,'authenticationPolicyList', ArrayOf(AuthenticationPolicy))
+        , (OptionalProperty,'authenticationPolicyNames', ArrayOf(CharacterString))
+        , (ReadableProperty,'authorizationMode', AuthorizationMode)
+        , (OptionalProperty,'verificationTime', Unsigned)
+        , (OptionalProperty,'lockout', Boolean)
+        , (OptionalProperty,'lockoutRelinquishTime', Unsigned)
+        , (OptionalProperty,'failedAttempts', Unsigned)
+        , (OptionalProperty,'failedAttemptEvents', SequenceOf(AccessEvent))
+        , (OptionalProperty,'maxFailedAttempts', Unsigned)
+        , (OptionalProperty,'failedAttemptsTime', Unsigned)
+        , (OptionalProperty,'threatLevel', AccessThreatLevel)
+        , (OptionalProperty,'occupancyUpperLimitEnforced', Boolean)
+        , (OptionalProperty,'occupancyLowerLimitEnforced', Boolean)
+        , (OptionalProperty,'occupancyCountAdjust', Boolean)
+        , (OptionalProperty,'accompanimentTime', Unsigned)
+        , (ReadableProperty,'accessEvent', AccessEvent)
+        , (ReadableProperty,'accessEventTag', Unsigned)
+        , (ReadableProperty,'accessEventTime', TimeStamp)
+        , (ReadableProperty,'accessEventCredential', DeviceObjectReference)
+        , (OptionalProperty,'accessEventAuthenticationFactor', AuthenticationFactor)
+        , (ReadableProperty,'accessDoors', ArrayOf(DeviceObjectReference))
+        , (ReadableProperty,'priorityForWriting', Unsigned)
+        , (OptionalProperty,'musterPoint', Boolean)
+        , (OptionalProperty,'zoneTo', DeviceObjectReference)
+        , (OptionalProperty,'zoneFrom', DeviceObjectReference)
+        , (OptionalProperty,'notificationClass', Unsigned)
+        , (OptionalProperty,'transactionNotificationClass', Unsigned)
+        , (OptionalProperty,'accessAlarmEvents', SequenceOf(AccessEvent))
+        , (OptionalProperty,'accessTransactionEvents', SequenceOf(AccessEvent))
+        , (OptionalProperty,'eventEnable', EventTransitionBits)
+        , (OptionalProperty,'ackedTransitions', EventTransitionBits)
+        , (OptionalProperty,'notifyType', NotifyType)
+        , (OptionalProperty,'eventTimeStamps', ArrayOf(TimeStamp))
+        , (OptionalProperty,'eventMessageTexts', ArrayOf(CharacterString))
+        , (OptionalProperty,'eventMessageTextsConfig', ArrayOf(CharacterString))
+        , (OptionalProperty,'eventDetectionEnable', Boolean)
+        , (OptionalProperty,'eventAlgorithmInhibitRef', ObjectPropertyReference)
+        , (OptionalProperty,'eventAlgorithmInhibit', Boolean)
+        , (OptionalProperty,'reliabilityEvaluationInhibit', Boolean)
+        ]
+        
+    def test_object(self):
+        self.object_type(self.obj,self.objType)
+        self.object_number_of_properties(self.obj,self.numberOfPropertiesRequired)
+        self.object_property_identifiers(self.obj)
+        self.object_property_dataType(self.obj)
+        self.object_noWritingToReadableProperty(self.obj,self.writeValue)
+        self.object_can_write_to_writableProperty(self.obj,self.writeValue)
+        self.object_can_read_property(self.obj)
+        
