e553a362dd898ccd2918d4c7b978153f82d720f4
Christian Tremblay, ing <christian.tremblay@servisys.com>
2015-08-13 22:01:10 -0400

    Added tests for AccessRightObject, AccessUserObject, AccessZoneObject, AccumulatorObject,AlertEnrollmentObject,AnalogInputObject, AnalogOutputObject,AnalogValueObject
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

---
+++ tests/test_objects/test_AccessRightsObject.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Nose Test Objects AccessPointObject
+---------------------
+"""
+
+import unittest
+from helper import TestObjectHelper
+
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger, xtob
+from bacpypes.object import AccessRightsObject, WritableProperty, ReadableProperty, OptionalProperty
+from bacpypes.primitivedata import BitString, Boolean, CharacterString, Date, Double, \
+    Enumerated, Integer, Null, ObjectIdentifier, OctetString, Real, Time, \
+    Unsigned
+from bacpypes.constructeddata import AnyAtomic, Array, ArrayOf, Choice, Element, \
+    Sequence, SequenceOf
+from bacpypes.basetypes import AccessCredentialDisable, AccessCredentialDisableReason, \
+    AccessEvent, AccessPassbackMode, AccessRule, AccessThreatLevel, \
+    AccessUserType, AccessZoneOccupancyState, AccumulatorRecord, Action, \
+    ActionList, AddressBinding, AssignedAccessRights, AuthenticationFactor, \
+    AuthenticationFactorFormat, AuthenticationPolicy, AuthenticationStatus, \
+    AuthorizationException, AuthorizationMode, BackupState, BinaryPV, \
+    COVSubscription, CalendarEntry, ChannelValue, ClientCOV, \
+    CredentialAuthenticationFactor, DailySchedule, DateRange, DateTime, \
+    Destination, DeviceObjectPropertyReference, DeviceObjectReference, \
+    DeviceStatus, DoorAlarmState, DoorSecuredStatus, DoorStatus, DoorValue, \
+    EngineeringUnits, EventNotificationSubscription, EventParameter, \
+    EventState, EventTransitionBits, EventType, FaultParameter, FaultType, \
+    FileAccessMethod, LifeSafetyMode, LifeSafetyOperation, LifeSafetyState, \
+    LightingCommand, LightingInProgress, LightingTransition, LimitEnable, \
+    LockStatus, LogMultipleRecord, LogRecord, LogStatus, LoggingType, \
+    Maintenance, NetworkSecurityPolicy, NodeType, NotifyType, \
+    ObjectPropertyReference, ObjectTypesSupported, OptionalCharacterString, \
+    Polarity, PortPermission, Prescale, PriorityArray, ProcessIdSelection, \
+    ProgramError, ProgramRequest, ProgramState, PropertyAccessResult, \
+    PropertyIdentifier, Recipient, Reliability, RestartReason, Scale, \
+    SecurityKeySet, SecurityLevel, Segmentation, ServicesSupported, \
+    SetpointReference, ShedLevel, ShedState, SilencedState, SpecialEvent, \
+    StatusFlags, TimeStamp, VTClass, VTSession, WriteStatus
+from bacpypes.errors import ConfigurationError, ExecutionError
+
+# some debugging
+_debug = 1
+_log = ModuleLogger(globals())
+
+@bacpypes_debugging
+class Test_AccessRightsObject(unittest.TestCase, TestObjectHelper):
+    """
+    This test will verify that the object created has good number of properties
+    Will test that each property is correct datatype
+    Will test that only Writable property can be written
+    Will test that all properties can be read
+    """
+    def setUp(self):
+        if _debug: Test_AccessRightsObject._debug("Test_AccessRightsObject")
+        self.obj = AccessRightsObject()
+        self.objType = 'accessRights'
+        self.identifiers = self.build_list_of_identifiers(self.obj.properties)
+        self.numberOfPropertiesRequired = 8
+        self.writeValue = 0
+        self.listOfProperties = \
+        [ (WritableProperty,'globalIdentifier', Unsigned)
+        , (ReadableProperty,'statusFlags', StatusFlags)
+        , (ReadableProperty,'reliability', Reliability)
+        , (ReadableProperty,'enable', Boolean)
+        , (ReadableProperty,'negativeAccessRules', ArrayOf(AccessRule))
+        , (ReadableProperty,'positiveAccessRules', ArrayOf(AccessRule))
+        , (OptionalProperty,'accompaniment', DeviceObjectReference)
+        , (OptionalProperty,'reliabilityEvaluationInhibit', Boolean)
+        ]
+        
+    def test_object(self):
+        self.object_type(self.obj,self.objType)
+        self.object_number_of_properties(self.obj,self.numberOfPropertiesRequired)
+        self.object_property_identifiers(self.obj)
+        self.object_property_dataType(self.obj)
+        self.object_noWritingToReadableProperty(self.obj,self.writeValue)
+        self.object_can_write_to_writableProperty(self.obj,self.writeValue)
+        self.object_can_read_property(self.obj)
+        
