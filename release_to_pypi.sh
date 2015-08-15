4b163d27f0c5713147584045438b304f7d5d7506
Joel Bender <jjb5@cornell.edu>
2015-08-14 21:08:31 -0400

    no need to run setup as a superuser

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+python setup.py sdist
+
 for ver in 2.7 3.4; do
-    sudo python$ver setup.py bdist_egg
-    sudo python$ver setup.py bdist_wheel
+    python$ver setup.py bdist_egg
+    python$ver setup.py bdist_wheel
 done
 twine upload dist/*
 
