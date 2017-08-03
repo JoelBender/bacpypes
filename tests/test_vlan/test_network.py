ce0b176e70e991bfe179bce1ceb2ae38df5184fa
Joel Bender <joel@carrickbender.com>
2017-08-03 14:54:53 -0400

    trying out test code pattern

--- tests/test_vlan/test_network.py
+++ tests/test_vlan/test_network.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Test Module Template
---------------------
+Test Network
+------------
+
+This module tests the basic functionality of a VLAN network.  Each test "runs"
+on a VLAN with two nodes, node_1 and node_2, and each has a state machine.
 """
 
 import unittest
 
 from bacpypes.debugging import bacpypes_debugging, ModuleLogger
+
+from bacpypes.pdu import Address, PDU
+from bacpypes.comm import bind
+from bacpypes.vlan import Network, Node
+
+from ..state_machine import ClientStateMachine, StateMachineGroup
+from ..time_machine import reset_time_machine, run_time_machine
 
 # some debugging
 _debug = 0
@@ -16,48 +26,104 @@ _log = ModuleLogger(globals())
 
 
 @bacpypes_debugging
-def setup_module():
-    if _debug: setup_module._debug("setup_module")
+class ZPDU():
+
+    def __init__(self, cls=None, **kwargs):
+        if _debug: ZPDU._debug("__init__ %r", kwargs)
+
+        self.cls = cls
+        self.kwargs = kwargs
+
+    def __eq__(self, pdu):
+        if _debug: ZPDU._debug("__eq__ %r", pdu)
+
+        # match the object type if it was provided
+        if self.cls is not None:
+            if not isinstance(pdu, self.cls):
+                if _debug: ZPDU._debug("    - wrong class")
+                return False
+
+        # match the attribute names and values
+        for k, v in self.kwargs.items():
+            if not hasattr(pdu, k):
+                if _debug: ZPDU._debug("    - missing attribute: %r", k)
+                return False
+            if getattr(pdu, k) != v:
+                if _debug: ZPDU._debug("    - %s value: %r", k, v)
+                return False
+
+        # nothing failed
+        return True
 
 
 @bacpypes_debugging
-def teardown_module():
-    if _debug: teardown_module._debug("teardown_module")
+class ZStateMachineGroup(StateMachineGroup):
+
+    def run(self, time_limit=60.0):
+        if _debug: ZStateMachineGroup._debug("run %r", time_limit)
+
+        # reset the time machine
+        reset_time_machine()
+        if _debug: ZStateMachineGroup._debug("    - time machine reset")
+
+        # run the group
+        super(ZStateMachineGroup, self).run()
+
+        # run it for some time
+        run_time_machine(time_limit)
+        if _debug: ZStateMachineGroup._debug("    - time machine finished")
+
+        # check for success
+        all_success, some_failed = super(ZStateMachineGroup, self).check_for_success()
+        assert all_success
 
 
 @bacpypes_debugging
-def setup_function(function):
-    if _debug: setup_function._debug("setup_function %r", function)
-
-
-@bacpypes_debugging
-def teardown_function(function):
-    if _debug: teardown_function._debug("teardown_function %r", function)
-
-
-@bacpypes_debugging
-class TestCaseTemplate(unittest.TestCase):
-
-    @classmethod
-    def setup_class(cls):
-        if _debug: TestCaseTemplate._debug("setup_class")
-
-    @classmethod
-    def teardown_class(cls):
-        if _debug: TestCaseTemplate._debug("teardown_class")
+class TestVLAN(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
-        if _debug: TestCaseTemplate._debug("__init__ %r %r", args, kwargs)
-        super(TestCaseTemplate, self).__init__(*args, **kwargs)
+        if _debug: TestVLAN._debug("__init__ %r %r", args, kwargs)
+        super(TestVLAN, self).__init__(*args, **kwargs)
 
-    def setup_method(self, method):
-        if _debug: TestCaseTemplate._debug("setup_method %r", method)
+        # create a network and bound nodes
+        self.vlan = Network()
+        self.node_1 = Node(Address(1), self.vlan)
+        self.node_2 = Node(Address(2), self.vlan)
 
-    def teardown_method(self, method):
-        if _debug: TestCaseTemplate._debug("teardown_method %r", method)
+        # create state machines and bind them to nodes
+        self.csm_1 = ClientStateMachine()
+        bind(self.csm_1, self.node_1)
+        self.csm_2 = ClientStateMachine()
+        bind(self.csm_2, self.node_2)
 
-    def test_something(self):
-        if _debug: TestCaseTemplate._debug("test_something")
+        # create a state machine group so they both run together
+        self.csm_group = ZStateMachineGroup()
+        self.csm_group.append(self.csm_1)
+        self.csm_group.append(self.csm_2)
 
-    def test_something_else(self):
-        if _debug: TestCaseTemplate._debug("test_something_else")
+    def test_idle(self):
+        if _debug: TestVLAN._debug("test_idle")
+
+        # make a send transition from start to success, run the machine
+        self.csm_1.start_state.success()
+        self.csm_2.start_state.success()
+
+        # run the group
+        self.csm_group.run()
+
+    def test_send_receive(self):
+        if _debug: TestVLAN._debug("test_send_receive")
+
+        # make a PDU from node 1 to node 2
+        pdu = PDU(b'data', source=self.node_1.address, destination=self.node_2.address)
+        if _debug: TestVLAN._debug("    - pdu: %r", pdu)
+
+        # make a send transition from start to success, run the machine
+        self.csm_1.start_state.send(pdu).success()
+        self.csm_2.start_state.receive(ZPDU(
+            pduSource=Address(1),
+            )).success()
+
+        # run the group
+        self.csm_group.run()
+
