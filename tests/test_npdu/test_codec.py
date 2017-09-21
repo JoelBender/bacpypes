3459dad17fd3e69a7cd7270d80895576fff6f0f1
Joel Bender <joel@carrickbender.com>
2017-09-20 21:01:46 -0400

    finish the codec tests

--- tests/test_npdu/test_codec.py
+++ tests/test_npdu/test_codec.py
@@ -296,3 +296,103 @@ class TestNPDUCodec(unittest.TestCase):
         self.response(PDU(pdu_bytes))
         self.confirmation(InitializeRoutingTable, irtTable=rt_entries)
 
+    def test_initialize_routing_table_ack_01(self):
+        """Test the InitializeRoutingTableAck with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_initialize_routing_table_ack_01")
+
+        # build a routing table entry
+        rte = RoutingTableEntry(1, 2, xtob(''))
+        rt_entries = [rte]
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '07 01'                     # message type and list length
+                '0001 02 00'            # network, port number, port info
+            )
+
+        self.request(InitializeRoutingTableAck(rt_entries))
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(InitializeRoutingTableAck, irtaTable=rt_entries)
+
+    def test_initialize_routing_table_ack_02(self):
+        """Test the RouterAvailableToNetwork with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_initialize_routing_table_ack_02")
+
+        # build a routing table entry
+        rte = RoutingTableEntry(3, 4, xtob('deadbeef'))
+        rt_entries = [rte]
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '07 01'                     # message type and list length
+                '0003 04 04 DEADBEEF'   # network, port number, port info
+            )
+
+        self.request(InitializeRoutingTableAck(rt_entries))
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(InitializeRoutingTableAck, irtaTable=rt_entries)
+
+    def test_establish_connection_to_network(self):
+        """Test the EstablishConnectionToNetwork with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_establish_connection_to_network")
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '08 0005 06'                # message type, network, termination time
+            )
+
+        self.request(EstablishConnectionToNetwork(5, 6))
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(EstablishConnectionToNetwork, ectnDNET=5, ectnTerminationTime=6)
+
+    def test_disconnect_connection_to_network(self):
+        """Test the DisconnectConnectionToNetwork with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_disconnect_connection_to_network")
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '09 0007'                   # message type, network
+            )
+
+        self.request(DisconnectConnectionToNetwork(7))
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(DisconnectConnectionToNetwork, dctnDNET=7)
+
+    def test_what_is_network_number(self):
+        """Test the WhatIsNetworkNumber with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_what_is_network_number")
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '12'                        # message type
+            )
+
+        self.request(WhatIsNetworkNumber())
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(WhatIsNetworkNumber)
+
+    def test_network_number_is(self):
+        """Test the NetworkNumberIs with a routing table entry."""
+        if _debug: TestNPDUCodec._debug("test_network_number_is")
+
+        # Request successful
+        pdu_bytes = xtob('01.80'        # version, network layer message
+            '13 0008 01'                # message type, network, flag
+            )
+
+        self.request(NetworkNumberIs(8, 1))
+        self.indication(pduData=pdu_bytes)
+
+        self.response(PDU(pdu_bytes))
+        self.confirmation(NetworkNumberIs, nniNet=8, nniFlag=1)
+
