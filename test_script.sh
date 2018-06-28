5c3c13346a45e15c62ba4210e2b86c288f04dd7a
Joel Bender <joel@betelgeuse>
2018-06-28 12:23:08 -0400

    add a sample script to run tests -- not sure where to put this

---
+++ test_script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+#   BACpypes Test Script
+#
+#   This is an example script for running specific tests
+#   and capturing the debugging output where it can be
+#   searched for exceptions, errors, failed tests, etc.
+#
+
+version=3
+options=""
+
+while getopts v:o: OPTION
+do
+    case $OPTION in
+        v)
+            # which python version
+            version=$OPTARG
+            ;;
+        o)
+            options=$OPTARG
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# this is where debugging output should go, the name of the
+# file matches the name of the script
+bugfile=$(basename $0)
+bugfile=${bugfile/.sh/.txt}
+
+# debugging file can rotate, set the file size large to keep
+# it from rotating a lot
+export BACPYPES_MAXBYTES=10485760
+
+# add the modules or classes that need debugging and redirect
+# the output to the file
+export BACPYPES_DEBUG=" \
+    tests.test_service.helpers.ApplicationNetwork:$bugfile \
+    tests.test_service.helpers.SnifferStateMachine:$bugfile \
+    tests.state_machine.match_pdu:$bugfile \
+    "
+
+# debugging output will open the file 'append' which is
+# not very helpful in most cases, remove the existing debugging file
+rm -vf $bugfile
+
+# run the tests for a specific file, the additional options
+# are passed to pytest
+python$version setup.py test --addopts "tests/test_service/test_cov.py $options"
+
+# if all the tests pass, remove the debugging output, otherwise
+# display for your enjoyment
+if [ $? -eq 0 ]
+then
+    rm -vf $bugfile
+else
+    less $bugfile
+fi
