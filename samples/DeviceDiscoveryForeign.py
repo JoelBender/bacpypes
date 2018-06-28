ac840a13da7e5ea6fb9146b68e0e60695bcaa43b
Joel Bender <joel@betelgeuse>
2018-06-27 20:42:33 -0400

    update to the API changes

--- samples/DeviceDiscoveryForeign.py
+++ samples/DeviceDiscoveryForeign.py
@@ -171,16 +171,12 @@ class DiscoveryConsoleCmd(ConsoleCmd):
         args = args.split()
         if _debug: DiscoveryConsoleCmd._debug("do_rtn %r", args)
 
-        # safe to assume only one adapter
-        adapter = this_application.nsap.adapters[0]
-        if _debug: DiscoveryConsoleCmd._debug("    - adapter: %r", adapter)
-
         # provide the address and a list of network numbers
         router_address = Address(args[0])
         network_list = [int(arg) for arg in args[1:]]
 
         # pass along to the service access point
-        this_application.nsap.add_router_references(adapter, router_address, network_list)
+        this_application.nsap.add_router_references(None, router_address, network_list)
 
 
 #
