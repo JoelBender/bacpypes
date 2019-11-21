097435b47503cefa7b35aa7a58e587bd8f859aa3
Safrone <eblumster@gmail.com>
2019-11-21 15:21:43 -0500

    Fix issue with Accumulator Scale property (#302)
    - add context numbers to choiceElements
    - create test for Scale decode

--- tests/test_constructed_data/test_choice.py
+++ tests/test_constructed_data/test_choice.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# placeholder
+"""
+Test Choice
+-----------
+"""
+
+import unittest
+
+from bacpypes.basetypes import Scale
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger
+from bacpypes.primitivedata import Tag, TagList
+
+# some debugging
+_debug = 0
+_log = ModuleLogger(globals())
+
+
+@bacpypes_debugging
+class TestScaleChoice(unittest.TestCase):
+    def test_scale_choice(self):
+        if _debug: TestScaleChoice._debug("test_scale_choice")
+        taglist = TagList([Tag(1, 1, 1, bytearray(b'\x00'))])
+        scale = Scale()
+        scale.decode(taglist)
+        self.assertDictEqual(scale.dict_contents(), {'integerScale': 0})
