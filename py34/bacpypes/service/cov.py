859cb49c9be9e514afd70fce428322f86aac7476
Christian Tremblay, ing <christian.tremblay@servisys.com>
2020-10-25 20:32:58 -0400

    Changed the way we handle an empty lifetime in APDU looking for None instead of 0 to fix a Runtime error when install_task gets a delta of None

--- py34/bacpypes/service/cov.py
+++ py34/bacpypes/service/cov.py
@@ -102,11 +102,10 @@ class Subscription(OneShotTask, DebugContents):
         self.proc_id = proc_id
         self.obj_id = obj_id
         self.confirmed = confirmed
-        self.lifetime = lifetime
 
-        # if lifetime is non-zero, schedule the subscription to expire
-        if lifetime != 0:
-            self.install_task(delta=self.lifetime)
+        # if lifetime is none, consider permanent subscription (0)        
+        self.lifetime = 0 if lifetime is None else lifetime
+        self.install_task(delta=self.lifetime)
 
     def cancel_subscription(self):
         if _debug: Subscription._debug("cancel_subscription")
