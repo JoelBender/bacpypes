54a0614cb7228d28fa8a81fb3c313e845ccf7633
Joel Bender <joel@betelgeuse>
2024-05-13 01:17:35 -0400

    bump the version

--- py34/bacpypes/__init__.py
+++ py34/bacpypes/__init__.py
@@ -18,7 +18,7 @@ if _sys.platform not in _supported_platforms:
 #   Project Metadata
 #
 
-__version__ = '0.18.7'
+__version__ = '0.18.8'
 __author__ = 'Joel Bender'
 __email__ = 'joel@carrickbender.com'
 
